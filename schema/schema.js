f33397229981897c2ed63fddbf2474ed5eae105b
talitast <talitatravassos.tt@gmail.com>
2019-10-22 11:40:35 -0300

    join relate data

--- schema/schema.js
+++ schema/schema.js
@@ -10,13 +10,13 @@ const {
 } = graphql;
 
 // dummy data
-var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+let books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
 ];
 
-var authors = [
+let authors = [
     { name: 'Patrick Rothfuss', age: 44, id: '1' },
     { name: 'Brandon Sanderson', age: 42, id: '2' },
     { name: 'Terry Pratchett', age: 66, id: '3' }
@@ -27,7 +27,13 @@ const BookType = new GraphQLObjectType({
     fields: ( ) => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: { 
+          type: AuthorType,
+          resolve( parent, args){
+            return _.find( authors, { id: parent.authorId })
+          }
+         }
     })
 });
 
